f3917d214202ac0aabbad01f1d45ca0586065b16
juniorug <junior@daterraweb.com>
2017-07-16 16:16:13 -0300

    finalizando

---
+++ FaceTrackingViewer.xaml.cs
@@ -0,0 +1,412 @@
+﻿
+namespace FaceTrackingBasics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.Toolkit.FaceTracking;
+
+    using Point = System.Windows.Point;
+
+    /// <summary>
+    /// Class that uses the Face Tracking SDK to display a face mask for
+    /// tracked skeletons
+    /// </summary>
+    public partial class FaceTrackingViewer : UserControl, IDisposable
+    {
+        public static readonly DependencyProperty KinectProperty = DependencyProperty.Register(
+            "Kinect", 
+            typeof(KinectSensor), 
+            typeof(FaceTrackingViewer), 
+            new PropertyMetadata(
+                null, (o, args) => ((FaceTrackingViewer)o).OnSensorChanged((KinectSensor)args.OldValue, (KinectSensor)args.NewValue)));
+
+        private const uint MaxMissedFrames = 100;
+
+        private readonly Dictionary<int, SkeletonFaceTracker> trackedSkeletons = new Dictionary<int, SkeletonFaceTracker>();
+        
+        private readonly Dictionary<int, int> skeletonIdMap = new Dictionary<int, int>();
+
+        private byte[] colorImage;
+
+        private ColorImageFormat colorImageFormat = ColorImageFormat.Undefined;
+
+        private short[] depthImage;
+
+        private DepthImageFormat depthImageFormat = DepthImageFormat.Undefined;
+
+        private bool disposed;
+
+        private Skeleton[] skeletonData;
+
+        public FaceTrackingViewer()
+        {
+            this.InitializeComponent();
+        }
+
+        ~FaceTrackingViewer()
+        {
+            this.Dispose(false);
+        }
+
+        public KinectSensor Kinect
+        {
+            get
+            {
+                return (KinectSensor)this.GetValue(KinectProperty);
+            }
+
+            set
+            {
+                this.SetValue(KinectProperty, value);
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                this.ResetFaceTracking();
+
+                this.disposed = true;
+            }
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+           
+            foreach (KeyValuePair<int, SkeletonFaceTracker> item in this.trackedSkeletons)
+            {
+                int i = item.Key;
+                SkeletonFaceTracker faceInformation = item.Value;
+                faceInformation.DrawFaceModel(drawingContext, i);
+
+            }
+        }
+
+        private void OnAllFramesReady(object sender, AllFramesReadyEventArgs allFramesReadyEventArgs)
+        {
+            ColorImageFrame colorImageFrame = null;
+            DepthImageFrame depthImageFrame = null;
+            SkeletonFrame skeletonFrame = null;
+
+            try
+            {
+                colorImageFrame = allFramesReadyEventArgs.OpenColorImageFrame();
+                depthImageFrame = allFramesReadyEventArgs.OpenDepthImageFrame();
+                skeletonFrame = allFramesReadyEventArgs.OpenSkeletonFrame();
+
+                if (colorImageFrame == null || depthImageFrame == null || skeletonFrame == null)
+                {
+                    return;
+                }
+
+                if (this.depthImageFormat != depthImageFrame.Format)
+                {
+                    this.ResetFaceTracking();
+                    this.depthImage = null;
+                    this.depthImageFormat = depthImageFrame.Format;
+                }
+
+                if (this.colorImageFormat != colorImageFrame.Format)
+                {
+                    this.ResetFaceTracking();
+                    this.colorImage = null;
+                    this.colorImageFormat = colorImageFrame.Format;
+                }
+
+                // Create any buffers to store copies of the data we work with
+                if (this.depthImage == null)
+                {
+                    this.depthImage = new short[depthImageFrame.PixelDataLength];
+                }
+
+                if (this.colorImage == null)
+                {
+                    this.colorImage = new byte[colorImageFrame.PixelDataLength];
+                }
+                
+                // Get the skeleton information
+                if (this.skeletonData == null || this.skeletonData.Length != skeletonFrame.SkeletonArrayLength)
+                {
+                    this.skeletonData = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                }
+
+                colorImageFrame.CopyPixelDataTo(this.colorImage);
+                depthImageFrame.CopyPixelDataTo(this.depthImage);
+                skeletonFrame.CopySkeletonDataTo(this.skeletonData);
+
+                // Update the list of trackers and the trackers with the current frame information
+                foreach (Skeleton skeleton in this.skeletonData)
+                {
+                    if (skeleton.TrackingState == SkeletonTrackingState.Tracked
+                        || skeleton.TrackingState == SkeletonTrackingState.PositionOnly)
+                    {
+                        // We want keep a record of any skeleton, tracked or untracked.
+                        if (!this.trackedSkeletons.ContainsKey(skeleton.TrackingId))
+                        {
+                            this.trackedSkeletons.Add(skeleton.TrackingId, new SkeletonFaceTracker());
+                        }
+
+                        // Give each tracker the upated frame.
+                        SkeletonFaceTracker skeletonFaceTracker;
+                        if (this.trackedSkeletons.TryGetValue(skeleton.TrackingId, out skeletonFaceTracker))
+                        {
+                            skeletonFaceTracker.OnFrameReady(this.Kinect, colorImageFormat, colorImage, depthImageFormat, depthImage, skeleton);
+                            skeletonFaceTracker.LastTrackedFrame = skeletonFrame.FrameNumber;
+                        }
+                    }
+                }
+
+                this.RemoveOldTrackers(skeletonFrame.FrameNumber);
+
+                this.InvalidateVisual();
+            }
+            finally
+            {
+                if (colorImageFrame != null)
+                {
+                    colorImageFrame.Dispose();
+                }
+
+                if (depthImageFrame != null)
+                {
+                    depthImageFrame.Dispose();
+                }
+
+                if (skeletonFrame != null)
+                {
+                    skeletonFrame.Dispose();
+                }
+            }
+        }
+
+        private void OnSensorChanged(KinectSensor oldSensor, KinectSensor newSensor)
+        {
+            if (oldSensor != null)
+            {
+                oldSensor.AllFramesReady -= this.OnAllFramesReady;
+                this.ResetFaceTracking();
+            }
+
+            if (newSensor != null)
+            {
+                newSensor.AllFramesReady += this.OnAllFramesReady;
+            }
+        }
+
+        /// <summary>
+        /// Clear out any trackers for skeletons we haven't heard from for a while
+        /// </summary>
+        private void RemoveOldTrackers(int currentFrameNumber)
+        {
+            var trackersToRemove = new List<int>();
+
+            foreach (var tracker in this.trackedSkeletons)
+            {
+                uint missedFrames = (uint)currentFrameNumber - (uint)tracker.Value.LastTrackedFrame;
+                if (missedFrames > MaxMissedFrames)
+                {
+                    // There have been too many frames since we last saw this skeleton
+                    trackersToRemove.Add(tracker.Key);
+                }
+            }
+
+            foreach (int trackingId in trackersToRemove)
+            {
+                this.RemoveTracker(trackingId);
+            }
+        }
+
+        private void RemoveTracker(int trackingId)
+        {
+            this.trackedSkeletons[trackingId].Dispose();
+            this.trackedSkeletons.Remove(trackingId);
+            
+        }
+
+        private void ResetFaceTracking()
+        {
+            foreach (int trackingId in new List<int>(this.trackedSkeletons.Keys))
+            {
+                this.RemoveTracker(trackingId);
+            }
+        }
+
+        private class SkeletonFaceTracker : IDisposable
+        {
+            private static FaceTriangle[] faceTriangles;
+
+            private EnumIndexableCollection<FeaturePoint, PointF> facePoints;
+
+            private FaceTracker faceTracker;
+
+            private bool lastFaceTrackSucceeded;
+
+            private SkeletonTrackingState skeletonTrackingState;
+
+            public int LastTrackedFrame { get; set; }
+
+            public void Dispose()
+            {
+                if (this.faceTracker != null)
+                {
+                    this.faceTracker.Dispose();
+                    this.faceTracker = null;
+                }
+            }
+
+            public void DrawFaceModel(DrawingContext drawingContext, int personIndex)
+            {
+
+                if (!this.lastFaceTrackSucceeded || this.skeletonTrackingState != SkeletonTrackingState.Tracked)
+                {
+                    return;
+                }
+
+                var faceModelPts = new List<Point>();
+                var faceModel = new List<FaceModelTriangle>();
+
+                for (int i = 0; i < this.facePoints.Count; i++)
+                {
+                    faceModelPts.Add(new Point(this.facePoints[i].X + 0.5f, this.facePoints[i].Y + 0.5f));
+                }
+
+                foreach (var t in faceTriangles)
+                {
+                    var triangle = new FaceModelTriangle();
+                    triangle.P1 = faceModelPts[t.First];
+                    triangle.P2 = faceModelPts[t.Second];
+                    triangle.P3 = faceModelPts[t.Third];
+                    faceModel.Add(triangle);
+
+                }
+
+                var faceModelGroup = new GeometryGroup();
+                for (int i = 0; i < faceModel.Count; i++)
+                {
+                    var faceTriangle = new GeometryGroup();
+                    faceTriangle.Children.Add(new LineGeometry(faceModel[i].P1, faceModel[i].P2));
+                    faceTriangle.Children.Add(new LineGeometry(faceModel[i].P2, faceModel[i].P3));
+                    faceTriangle.Children.Add(new LineGeometry(faceModel[i].P3, faceModel[i].P1));
+                    faceModelGroup.Children.Add(faceTriangle);
+                }
+
+                SolidColorBrush[] colors = new SolidColorBrush[6] { Brushes.Blue, Brushes.Yellow, Brushes.Pink, Brushes.Green, Brushes.Pink, Brushes.Salmon };
+                drawingContext.DrawGeometry(colors[(personIndex % 6)], new Pen(colors[(personIndex % 6)], 1.0), faceModelGroup);
+            }
+
+            /// <summary>
+            /// Updates the face tracking information for this skeleton
+            /// </summary>
+            internal void OnFrameReady(KinectSensor kinectSensor, ColorImageFormat colorImageFormat, byte[] colorImage, DepthImageFormat depthImageFormat, short[] depthImage, Skeleton skeletonOfInterest)
+            {
+                this.skeletonTrackingState = skeletonOfInterest.TrackingState;
+
+                if (this.skeletonTrackingState != SkeletonTrackingState.Tracked)
+                {
+                    // nothing to do with an untracked skeleton.
+                    return;
+                }
+
+                if (this.faceTracker == null)
+                {
+                    try
+                    {
+                        this.faceTracker = new FaceTracker(kinectSensor);
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        // During some shutdown scenarios the FaceTracker
+                        // is unable to be instantiated.  Catch that exception
+                        // and don't track a face.
+                        Debug.WriteLine("AllFramesReady - creating a new FaceTracker threw an InvalidOperationException");
+                        this.faceTracker = null;
+                    }
+                }
+
+                if (this.faceTracker != null)
+                {
+                    FaceTrackFrame frame = this.faceTracker.Track(
+                        colorImageFormat, colorImage, depthImageFormat, depthImage, skeletonOfInterest);
+
+                    this.lastFaceTrackSucceeded = frame.TrackSuccessful;
+
+                    if (this.lastFaceTrackSucceeded)
+                    {
+                        if (faceTriangles == null)
+                        {
+                            // only need to get this once.  It doesn't change.
+                            faceTriangles = frame.GetTriangles();
+                        }
+                        this.facePoints = frame.GetProjected3DShape();
+
+                        PrintTrackedData(skeletonOfInterest);
+                    }
+                }
+            }
+
+            private void PrintTrackedData(Skeleton skeletonOfInterest)
+            {
+                SkeletonPoint currentPosition = skeletonOfInterest.Position;
+                int index = skeletonOfInterest.TrackingId;
+                String pattern = "0.000";
+                String distance = GetDistance(currentPosition).ToString(pattern);
+                String x = Math.Round(currentPosition.X, 3).ToString(pattern);
+                String y = Math.Round(currentPosition.Y, 3).ToString(pattern);
+                String z = Math.Round(currentPosition.Z, 3).ToString(pattern);
+                Debug.WriteLine(string.Format("[Person ID: {0}] [Distance: {1} m] [Coordinates: [X: {2}], [Y: {3}], [Z: {4}]]", index, distance,x,y,z));
+            }
+
+
+            private double GetDistance(SkeletonPoint point)
+            {
+
+                return Math.Round(
+                    Math.Sqrt(
+                        Math.Pow(point.X, 2) +
+                        Math.Pow(point.Y, 2) +
+                        Math.Pow(point.Z, 2)
+                    ), 
+                3);
+            }
+
+            private double Length(CameraSpacePoint point)
+            {
+                
+                return Math.Sqrt(
+                    point.X * point.X +
+                    point.Y * point.Y +
+                    point.Z * point.Z
+                );
+            }
+
+
+            public struct CameraSpacePoint {
+                  public double X ;
+                  public double Y;
+                  public double Z;
+            }     
+
+
+
+            private struct FaceModelTriangle
+            {
+                public Point P1;
+                public Point P2;
+                public Point P3;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,218 @@
+﻿
+namespace FaceTrackingBasics
+{
+    using System;
+    using System.Diagnostics;
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.Toolkit;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly int Bgr32BytesPerPixel = (PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
+        private readonly KinectSensorChooser sensorChooser = new KinectSensorChooser();
+        private WriteableBitmap colorImageWritableBitmap;
+        private byte[] colorImageData;
+        private ColorImageFormat currentColorImageFormat = ColorImageFormat.Undefined;
+
+        private KinectSensor sensor;
+        private WriteableBitmap colorBitmap;
+        private DepthImagePixel[] depthPixels;
+        private byte[] colorPixels;
+
+        private Skeleton[] skeletonData;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var faceTrackingViewerBinding = new Binding("Kinect") { Source = sensorChooser };
+            faceTrackingViewer.SetBinding(FaceTrackingViewer.KinectProperty, faceTrackingViewerBinding);
+
+            sensorChooser.KinectChanged += SensorChooserOnKinectChanged;
+
+            sensorChooser.Start();
+        }
+
+        private void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs kinectChangedEventArgs)
+        {
+            KinectSensor oldSensor = kinectChangedEventArgs.OldSensor;
+            KinectSensor newSensor = kinectChangedEventArgs.NewSensor;
+
+            if (oldSensor != null)
+            {
+                oldSensor.AllFramesReady -= KinectSensorOnAllFramesReady;
+                oldSensor.ColorStream.Disable();
+                oldSensor.DepthStream.Disable();
+                oldSensor.DepthStream.Range = DepthRange.Default;
+                oldSensor.SkeletonStream.Disable();
+                oldSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                oldSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Default;
+            }
+
+            if (newSensor != null)
+            {
+                try
+                {
+                    newSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                    newSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+                    try
+                    {
+                        // This will throw on non Kinect For Windows devices.
+                        newSensor.DepthStream.Range = DepthRange.Near;
+                        newSensor.SkeletonStream.EnableTrackingInNearRange = true;
+
+                        newSensor.SkeletonStream.Enable(); // Enable skeletal tracking
+
+                        this.skeletonData = new Skeleton[newSensor.SkeletonStream.FrameSkeletonArrayLength]; // Allocate ST data
+                        newSensor.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(kinect_SkeletonFrameReady); // Get Ready for Skeleton Ready Events
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        newSensor.DepthStream.Range = DepthRange.Default;
+                        newSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                    }
+
+                    newSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                    newSensor.SkeletonStream.Enable();
+                    newSensor.AllFramesReady += KinectSensorOnAllFramesReady;
+
+                    this.depthPixels = new DepthImagePixel[newSensor.DepthStream.FramePixelDataLength];
+
+                    // Allocate space to put the color pixels we'll create
+                    this.colorPixels = new byte[newSensor.DepthStream.FramePixelDataLength * sizeof(int)];
+
+                    // This is the bitmap we'll display on-screen
+                    this.colorBitmap = new WriteableBitmap(newSensor.DepthStream.FrameWidth, newSensor.DepthStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                    newSensor.DepthFrameReady += this.SensorDepthFrameReady;
+                }
+                catch (InvalidOperationException)
+                {
+                    Debug.WriteLine("InvalidOperationException");
+                }
+            }
+        }
+
+        private void WindowClosed(object sender, EventArgs e)
+        {
+            sensorChooser.Stop();
+            faceTrackingViewer.Dispose();
+        }
+
+        private void kinect_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame()) // Open the Skeleton frame
+            {
+                if (skeletonFrame != null && this.skeletonData != null) // check that a frame is available
+                {
+                    skeletonFrame.CopySkeletonDataTo(this.skeletonData); // get the skeletal information in this frame
+                    Debug.WriteLine(skeletonFrame.ToString());
+                }
+            }
+        }
+
+        private void KinectSensorOnAllFramesReady(object sender, AllFramesReadyEventArgs allFramesReadyEventArgs)
+        {
+            using (var colorImageFrame = allFramesReadyEventArgs.OpenColorImageFrame())
+            {
+                if (colorImageFrame == null)
+                {
+                    return;
+                }
+
+                var haveNewFormat = this.currentColorImageFormat != colorImageFrame.Format;
+                if (haveNewFormat)
+                {
+                    
+                    this.currentColorImageFormat = colorImageFrame.Format;
+                    this.colorImageData = new byte[colorImageFrame.PixelDataLength];
+                    this.colorImageWritableBitmap = new WriteableBitmap(colorImageFrame.Width, colorImageFrame.Height, 96, 96, PixelFormats.Bgr32, null);
+                    ColorImage.Source = this.colorBitmap;
+                    
+                }
+
+                colorImageFrame.CopyPixelDataTo(this.colorImageData);
+                this.colorImageWritableBitmap.WritePixels(
+                    new Int32Rect(0, 0, colorImageFrame.Width, colorImageFrame.Height),
+                    this.colorImageData,
+                    colorImageFrame.Width * Bgr32BytesPerPixel,
+                    0);
+            }
+        }
+
+        /// <summary>
+        /// Event handler for Kinect sensor's DepthFrameReady event
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void SensorDepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if (depthFrame != null)
+                {
+
+                    // Copy the pixel data from the image to a temporary array
+                    depthFrame.CopyDepthImagePixelDataTo(this.depthPixels);
+
+                    // Get the min and max reliable depth for the current frame
+                    int minDepth = depthFrame.MinDepth;
+                    int maxDepth = depthFrame.MaxDepth;
+
+                    // Convert the depth to RGB
+                    int colorPixelIndex = 0;
+                    for (int i = 0; i < this.depthPixels.Length; ++i)
+                    {
+                        // Get the depth for this pixel
+                        short depth = depthPixels[i].Depth;
+
+                        byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                        // Write out blue byte
+                        this.colorPixels[colorPixelIndex++] = intensity;
+
+                        // Write out green byte
+                        this.colorPixels[colorPixelIndex++] = intensity;
+
+                        // Write out red byte                        
+                        this.colorPixels[colorPixelIndex++] = intensity;
+
+                        // We're outputting BGR, the last byte in the 32 bits is unused so skip it
+                        // If we were outputting BGRA, we would write alpha here.
+                        ++colorPixelIndex;
+                    }
+
+                    // Write the pixel data into our bitmap
+                    this.colorBitmap.WritePixels(
+                        new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                        this.colorPixels,
+                        this.colorBitmap.PixelWidth * sizeof(int),
+                        0);
+                }
+            }
+        }
+
+
+        private void TrackPlayer(short[] depthFrame)
+        {
+            for (int depthIndex = 0, colorIndex = 0; depthIndex < depthFrame.Length; depthIndex++, colorIndex += 4)
+            {
+                int player = depthFrame[depthIndex] & DepthImageFrame.PlayerIndexBitmask;
+                if (player > 0)
+                {
+                    this.colorPixels[colorIndex + 2] = 169;
+                    this.colorPixels[colorIndex + 1] = 62;
+                    this.colorPixels[colorIndex + 0] = 9;
+                }
+            }
+        }
+
+    }
+}
